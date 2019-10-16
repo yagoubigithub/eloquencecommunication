eb76559a33b38764ba7ebbd5915f4f1e6f670982
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-10-16 10:59:37 +0200

    try to fix bugs

---
+++ js/home.js
@@ -0,0 +1,48 @@
+
+
+$(window).resize(function(){
+
+    if ($(window).width() < 576) {  
+
+           // is mobile device xs
+           $('#navbarNavAltMarkup').removeClass('justify-content-center')
+    }     
+
+});
+$(window).resize(function(){
+
+    if ($(window).width() >= 576) {  
+
+           // is mobile device
+           $('#navbarNavAltMarkup').removeClass('justify-content-center')
+    }     
+
+});
+$(window).resize(function(){
+
+    if ($(window).width() > 768) {  
+
+           // // Medium devices (tablets, 768px and up)
+           $('#navbarNavAltMarkup').removeClass('justify-content-center')
+    }     
+
+});
+$(window).resize(function(){
+
+    if ($(window).width() > 992) {  
+
+           /// Large devices (desktops, 992px and up)
+
+
+    }     
+
+});
+$(window).resize(function(){
+
+    if ($(window).width() > 1200) {  
+
+        // Extra large devices (large desktops, 1200px and up)
+
+    }     
+
+});
